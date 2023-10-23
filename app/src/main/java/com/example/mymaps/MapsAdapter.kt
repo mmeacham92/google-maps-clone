ab61c46023a68a09c9d9103bb1bebcdb9e3b4282
mmeacham92 <meacham.michael@rocketmail.com>
2023-10-23 13:10:43 -0500

    finished implementing methods within the adapter class; also created a userMaps variable from calling the provided getSampleData() method and now providing that list to the adapter constructor - we have a list of maps!

--- app/src/main/java/com/example/mymaps/MapsAdapter.kt
+++ app/src/main/java/com/example/mymaps/MapsAdapter.kt
@@ -1,22 +1,26 @@
 package com.example.mymaps
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mymaps.models.UserMap
 
 class MapsAdapter(val context: Context, val userMaps: List<UserMap>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(context).inflate(android.R.layout.simple_list_item_1, parent, false)
+
+        return ViewHolder(view)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount() = userMaps.size
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val userMap = userMaps[position]
+        val textViewTitle = holder.itemView.findViewById<TextView>(android.R.id.text1)
+        textViewTitle.text = userMap.title
     }
 
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
