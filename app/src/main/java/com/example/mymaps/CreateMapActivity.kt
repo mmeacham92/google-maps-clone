8c91c5232cf387d49b50d773839a3cb20a019af5
mmeacham92 <meacham.michael@rocketmail.com>
2023-10-25 13:36:03 -0500

    created dialog_create_place view which gets instantiated within the showAlertDialog method; an AlertDialog object is created which prompts for a title and description and has a cancel and OK button. When provided with valid input for both EditTexts and OK is pressed, a marker is dropped on that location and the corresponding data is populated on the marker - sweet!

--- app/src/main/java/com/example/mymaps/CreateMapActivity.kt
+++ app/src/main/java/com/example/mymaps/CreateMapActivity.kt
@@ -1,7 +1,12 @@
 package com.example.mymaps
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -10,6 +15,7 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.mymaps.databinding.ActivityCreateMapBinding
+import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.Marker
 
 class CreateMapActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -51,13 +57,47 @@ class CreateMapActivity : AppCompatActivity(), OnMapReadyCallback {
         // add a long click listener to the map
         // should get position from there
         mMap.setOnMapLongClickListener {latLng ->
-            val marker = mMap.addMarker(MarkerOptions().position(latLng).title("New Marker").snippet("generic description snippet"))
-            marker?.let { markers.add(it) }
+            showAlertDialog(latLng)
         }
 
         // Add a marker in Sydney and move the camera
 //        val sydney = LatLng(-34.0, 151.0)
 //        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
 //        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    private fun showAlertDialog(latLng: LatLng) {
+        // create a view instance for what the dialog box should look like?
+        val placeFormView = LayoutInflater.from(this).inflate(R.layout.dialog_create_place, null)
+
+        // create the actual alert dialog?
+        val dialog = AlertDialog.Builder(this)
+            .setTitle("Create a marker").setMessage("hello")
+            .setView(placeFormView)
+            .setNegativeButton("Cancel", null)
+            .setPositiveButton("OK", null)
+            .show()
+
+        // set a click listener on the positive button
+        dialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener {
+            val title = placeFormView.findViewById<EditText>(R.id.etTitle).text.toString()
+            val description = placeFormView.findViewById<EditText>(R.id.etDescription).text.toString()
+
+            if (title.trim().isEmpty() || description.trim().isEmpty()) {
+                Toast.makeText(this, "Place must have non-empty title and description", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            val marker = mMap.addMarker(
+                MarkerOptions().position(latLng).title(title)
+                    .snippet(description)
+            )
+
+            // essentially a null check on if marker is valid
+            marker?.let { markers.add(it) }
+
+            // close the alert dialog
+            dialog.dismiss()
+        }
     }
 }
